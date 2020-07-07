46539175052d10525e7dc637bb015e7b70d383f7
Joe Puzzo <jpuzzo@tesla.com>
2020-07-07 07:50:12 -0700

    Fist ever commit

---
+++ myscript.js
@@ -0,0 +1,8 @@
+
+
+
+document.getElementById("tinas-button").onclick = function() {myFunction()};
+
+function myFunction() {
+  document.getElementById("tinas-header").innerHTML = "YOU CLICKED ME!";
+}
